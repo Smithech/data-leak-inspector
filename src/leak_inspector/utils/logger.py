14a6fe2525747a4b3fee435e0d085f94577c3a0a
Smithetch <j0h4nn.10@gmail.com>
2026-04-24 11:02:53 -0500

    chore: remove utils module

--- src/leak_inspector/utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def get_logger(name: str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(levelname)s | %(name)s | %(message)s",
-    )
-    return logging.getLogger(name)
